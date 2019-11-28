4c4f6bf524b84addddf0dad1604425511d8aee2f
Iyron Woodhouse <iyronwoodhouse@gmail.com>
2019-11-27 18:27:57 -0800

    Smoke tests added

---
+++ src/components/LandingNav/LandingNav.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import LandingNav from './LandingNav';
+
+
+describe(`LandingNav component`, () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<LandingNav />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
